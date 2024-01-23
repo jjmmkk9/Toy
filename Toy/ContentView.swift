a38854c3b8f48fdfcbd0b83983043e5165aeadb5
jjmmkk9 <abcdqwerty0192@gmail.com>
2024-01-23 10:46:48 +0900

    Added Toy Project

---
+++ Toy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Toy
+//
+//  Created by 비긴어브랜드 on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Toy/ToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyApp.swift
+//  Toy
+//
+//  Created by 비긴어브랜드 on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

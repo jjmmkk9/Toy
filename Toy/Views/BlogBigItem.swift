f8ab6adb189802f2898b3c56c3e518c19f69882d
jjmmkk9 <abcdqwerty0192@gmail.com>
2024-02-02 14:49:23 +0900

    onTapGesture에 url로 사파리 오픈 - BlogBigItem destination property추가

--- Toy/Views/BlogBigItem.swift
+++ Toy/Views/BlogBigItem.swift
@@ -12,41 +12,45 @@ struct BlogBigItem: View {
     var btnColor: Color
     var image : String
     var isWhite: Bool
+    var destination: String
     
     var body: some View {
         
-        
-        HStack{
-            VStack (alignment: .leading, spacing: 8){
-                Text(text)
-                .font(.title2)
-                .bold()
-                .foregroundStyle(isWhite ? .white : .black)
-                .fixedSize(horizontal: true, vertical: true)
-                Text("자세히 보기")
-                    .foregroundStyle(isWhite ? Color(red: 211 / 255, green: 204 / 255, blue: 204 / 255) : .gray)
+            HStack{
+                VStack (alignment: .leading, spacing: 8){
+                    Text(text)
+                    .font(.title2)
+                    .bold()
+                    .foregroundStyle(isWhite ? .white : .black)
+                    .fixedSize(horizontal: true, vertical: true)
+                    Text("자세히 보기")
+                        .foregroundStyle(isWhite ? Color(red: 211 / 255, green: 204 / 255, blue: 204 / 255) : .gray)
+                }
+                Spacer()
+                Image(image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 70, height: 70)
+                    
             }
-            Spacer()
-            Image(image)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 70, height: 70)
-                
-        }
-        .padding(50)
-        
-        .background(
-            RoundedRectangle(cornerRadius: 25.0)
-            .foregroundStyle(btnColor)
-            .padding(20)
-            .background(Color(red: 247/255, green: 247/255, blue: 247/255))
-        )
-        .frame(height: 180)
-        
+            .padding(50)
+            .onTapGesture {
+                if let url = URL(string: destination){
+                    UIApplication.shared.open(url, options: [:])
+                }
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 25.0)
+                .foregroundStyle(btnColor)
+                .padding(20)
+                .background(Color(red: 247/255, green: 247/255, blue: 247/255))
+            )
+            .frame(height: 180)
             
     }
 }
 
+
 #Preview {
-    BlogBigItem(text: "클로바 노트,\n이럴때 유용해요!", btnColor: Color(red: 219 / 255, green: 208 / 255, blue: 123 / 255), image: "root", isWhite: false)
+    BlogBigItem(text: "클로바 노트,\n이럴때 유용해요!", btnColor: Color(red: 219 / 255, green: 208 / 255, blue: 123 / 255), image: "root", isWhite: false, destination: "https://www.apple.com")
 }
---
+++ Toy/Views/LoadingIndicator.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingIndicator.swift
+//  Toy
+//
+//  Created by 비긴어브랜드 on 2/2/24.
+//
+
+import SwiftUI
+
+class LoadingIndicator {
+    //매번 객체 생성 필요 없으니 static으로 선언
+    static func showLoading(){
+        DispatchQueue.main.async {
+//            guard let window = UIApplication.shared.window
+        }
+    }
+    
+    static func hideLoading() {
+        DispatchQueue.main.async {
+            
+        }
+    }
+}

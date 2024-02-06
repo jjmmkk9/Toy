2d0f1c59fc222f5e6a5a3e90ea83a83db44e0cde
jjmmkk9 <abcdqwerty0192@gmail.com>
2024-02-06 17:56:08 +0900

    기존 noteNothingView 제거하고 NoteView 내에서 사용할 걸로 새로 추출

--- Toy/Views/Folder/NoteView.swift
+++ Toy/Views/Folder/NoteView.swift
@@ -29,23 +29,9 @@ struct NoteView: View {
                     }
                 }
             }else {
-                VStack(spacing: 10){
-                    Image("manWithPencil")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100)
-                    Text("노트를 만들어 보세요.")
-                        .font(.title2)
-                        .bold()
-                    Text("녹음을 하거나 파일을 업로드하면")
-                    Text("텍스트로 확인할 수 있어요.")
-                }
-                .padding(.top, 180)
+                NoteNothingView()
             }
-                
 
-            
-            
             // 중앙 정렬을 위한 Spacer 추가
             Spacer()
 
@@ -56,6 +42,23 @@ struct NoteView: View {
         .background(Color(red: 246 / 255, green: 247 / 255, blue: 246 / 255))
 
 
+    }
+}
+
+private struct NoteNothingView : View{
+    var body: some View{
+        VStack(spacing: 10){
+            Image("manWithPencil")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100)
+            Text("노트를 만들어 보세요.")
+                .font(.title2)
+                .bold()
+            Text("녹음을 하거나 파일을 업로드하면")
+            Text("텍스트로 확인할 수 있어요.")
+        }
+        .padding(.top, 180)
     }
 }
 
